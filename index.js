ffbb4e91bf093bfb27a7c02c977317d931101749
John Szaszvari <jszaszvari@gmail.com>
2018-06-06 04:13:19 +1000

    update index

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const path = require('path')
 module.exports = (robot) => {
   const scriptsPath = path.resolve(__dirname, 'src')
   robot.loadFile(scriptsPath, 'diagnostics.js')
+  robot.loadFile(scriptsPath, 'version.js')
+  robot.loadFile(scriptsPath, 'example.js')
+
 }
